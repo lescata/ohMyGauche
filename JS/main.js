48c3b4d3656b6486bbc1dfd2abc9b6163cc64cab
lescata <alexandre.lescat@gmail.com>
2022-06-04 17:09:45 +0200

    samedi

--- JS/main.js
+++ JS/main.js
@@ -1,55 +1,152 @@
-const ctx = document.querySelector('canvas').getContext('2d')
+const $canvas = document.querySelector('canvas');
+const ctx = $canvas.getContext('2d');
 
-class Melanchon {
-  constructor() {
-    this.x = 50
-    this.y = 650
-    this.w = 50
-    this.h = 50
+const W = $canvas.width;
+const H = $canvas.height;
+
+const gravity = 2;
+const maxspeed= 30;
+
+const img = document.createElement('img')
+
+
+const backgroundImage = {
+  img: img,
+  x:-10, // commence direct a droite (evite un flash degeu)
+  speed: -1,
+
+  move: function() {
+    this.x += this.speed;
+    this.x %= canvas.width;
+  },
+
+  draw: function() {
+    ctx.drawImage(this.img, this.x,0);
+    ctx.drawImage(this.img, this.x + canvas.width, 0);
+  },
+};
+
+function updateCanvas() { 
+  backgroundImage.move();
+  backgroundImage.draw();
+}
+
+img.onload = updateCanvas;
+
+img.src = 'https://res.cloudinary.com/du5v6izdd/image/upload/v1654349713/Game/background1_nzrzkx.png';
+
+
+
+
+// test background// 
+var mimich = new Melanchon()
+
+function draw (){
+  ctx.clearRect(0, 0, W, H)//on commence par tous efface 
+  mimich.update()//on update la position de Melanchon
+  updateCanvas()
+  mimich.paint();//on imprime a lecran 
+}
+const pressed = {
+  arrowup: false,
+  arrowleft: false,
+  arrowright: false,//on set les touch forcement sur flase pour eviter la repetition
+}
+document.onkeydown = function (e) {
+  switch (e.keyCode) {
+ 
+    case 38:
+      if (pressed.arrowup) return; 
+      pressed.up = true;
+
+      mimich.jump(); 
+      break;
     
-    const img = document.createElement('img')
-    img.src = 'https://res.cloudinary.com/dtqr57xyj/image/upload/v1653500499/238-2382454_green-ghost-clipart-hd-png-download_copy.png'
-    
-    img.onload = () => {
-      console.log('Melanchon Download sucessfull')
-      this.draw()
-    }
-    
-    this.img = img
+    case 37:
+      if (pressed.arrowleft) return;
+      pressed.arrowleft = true;
+
+      mimich.backward(); 
+      break;
+
+    case 39:
+      if (pressed.arrowright) return; 
+      pressed.right = true;
+
+      mimich.forward(); 
+      break;
   }
-  
-  draw() {
-    ctx.drawImage(this.img, this.x, this.y)
+}
+/*document.onkeydown = function (el){
+  switch (el.keycode){
+    case 38:
+      if(pressed.arrowup) return;//if touche appuyer dont dot it
+      console.log('ici ca marche')// saute
+      pressed.up = true;
+      mimich.jump();
+      break;
+
+    case 37:
+      if (pressed.arrowleft)return;
+      console.log('ici ca marche')
+      pressed.arrowleft = true;// en avant 
+      mimich.backward();
+      break;
+
+    case 39:
+      if (pressed.arrowright) return;
+      console.log('ici ca marche')
+      pressed.arrowright = true;// en arriere 
+      mimich.forward();
+      break;
+  }
+}*/
+
+document.onkeyup = function (e) {
+  switch (e.keyCode) {
+    case 38:
+      pressed.arrowup = false;
+      break;
+
+      case 37:
+      pressed.arrowleft = false;
+      mimich.vx = 0;
+      break;
+
+    case 39:
+      pressed.arrowright = false;
+      mimich.vx = 0;
+      break;
   }
 }
 
+function animationLoop() {
+  draw();
+  requestAnimationFrame(animationLoop);
+}
+requestAnimationFrame(animationLoop)
 
 
-const melanchon1 = new Melanchon()
-console.log('fantom1=', melanchon1)
 
-document.addEventListener('keydown', function (event) {
+/*document.addEventListener('keydown', function (event) {
   console.log('keyCode=', event)
   
   if (event.key === 'ArrowRight') {
     ctx.clearRect(0,0,550,700)
-    melanchon1.x += 20
-    melanchon1.draw()
+    console.log('RIGHT baby')
+    mimich.x += 20
+    mimich.draw()
   }
   if (event.key === 'ArrowLeft') {
     ctx.clearRect(0,0,550,700)
-    melanchon1.x -= 20
-    melanchon1.draw()
-  }
-  
-  if (event.key === 'ArrowDown') {
-    ctx.clearRect(0,0,550,700)
-    melanchon1.y += 20
-    melanchon1.draw()
+    console.log('LEFT baby')
+    mimich.x -= 20
+    mimich.draw()
   }
   if (event.key === 'ArrowUp') {
     ctx.clearRect(0,0,550,700)
-    melanchon1.y -= 20
-    melanchon1.draw()
+    console.log('UP baby')
+    mimich.y -= 20
+    mimich.draw()
   }
-})
+})*/
--- JS/player.js
+++ JS/player.js
@@ -1,2 +1,47 @@
+class Melanchon {
+    constructor() {
+      this.x = 50    // coordoné X de la creation du joueur  
+      this.y = 650   // coordoné Y de la creation du joueur
+      this.w = 50
+      this.h = 110
+      
+      this.vx=0 //vitesse sur axe X
+      this.vy=0 //vitesse sur axe Y 
+    }
+  
+    jump() {
+      if (this.vy !== 0){ // ne saute pas si pas sur le sol
+        return
+      }
+      this.vy = -25;
+    }
+    forward() {
+        this.vx=5; //avance de 5px
+    }
+    backward(){
+        this.vx = -5;//recule de 5px
+    }
+    update(){
+        this.vy += gravity;
 
+        let sol= H;
+
+    if (this.vy > maxspeed){
+        this.vy = maxspeed;
+        }
+
+        this.x += this.vx;
+        this.y += this.vy;
+    
+    if (this.y + this.h > sol){ 
+        console.log('floor limit reach')
+        this.y = sol - this.h;
+        this.vy = 0;
+        }
+    }
+    
+    paint() {
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+        }
+  }
 
